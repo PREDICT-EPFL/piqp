7264ca2387d9dde20e4a552b8fb2a1142d37a03d
Roland Schwan <roland.schwan@mikrounix.com>
2023-01-25 15:18:10 +0100

    setup ci

---
+++ ci/shared/install_eigen.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This file is part of PIQP.
+#
+# Copyright (c) 2023 EPFL
+#
+# This source code is licensed under the BSD 2-Clause License found in the
+# LICENSE file in the root directory of this source tree.
+
+EIGEN_VERSION=${EIGEN_VERSION:-3.4.0};
+
+echo "Installing EIGEN..."
+
+git clone https://gitlab.com/libeigen/eigen.git eigen
+cd eigen
+git checkout "$EIGEN_VERSION"
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j2
+sudo make install
