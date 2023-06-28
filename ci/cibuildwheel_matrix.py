ad8938aad4a37be53e3f9cd0b3674690af75d71b
Roland Schwan <roland.schwan@mikrounix.com>
2023-06-28 12:06:16 +0200

    group targets by python version

--- ci/cibuildwheel_matrix.py
+++ ci/cibuildwheel_matrix.py
@@ -16,12 +16,22 @@ for platform in platforms:
     platform_targets = platform_targets.decode('utf-8')
     platform_targets = platform_targets.strip('\n').split('\n')
 
+    # group targets by python version
+    grouped_targets = {}
     for platform_target in platform_targets:
+        python_version = platform_target.split('-')[0]
+        if python_version not in grouped_targets:
+            grouped_targets[python_version] = []
+        grouped_targets[python_version].append(platform_target)
 
+    for python_version, group_targets in grouped_targets.items():
         targets.append({
+            'version': python_version,
             'os': runners[platform],
-            'build': platform_target,
+            'build': ' '.join(group_targets),
         })
+
+# print('matrix=' + json.dumps({'target': targets}))
 
 with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
     print('matrix=' + json.dumps({'target': targets}), file=fh)
