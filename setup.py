6524cb051237aeac68c854054c193fd343b214a3
Roland Schwan <roland.schwan@mikrounix.com>
2023-06-28 16:05:23 +0200

    prepare for 0.1.0 release

--- setup.py
+++ setup.py
@@ -142,7 +142,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="piqp",
-    version="0.0.1",
+    version="0.1.0",
     url='https://github.com/PREDICT-EPFL/piqp',
     author="Roland Schwan",
     author_email="roland.schwan@epfl.ch",
