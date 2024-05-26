5bbeb577bca35017da9ce4d3573de16b9065a00d
Joshua Redstone <redstone@gmail.com>
2024-05-26 16:37:11 -0400

    full

--- interfaces/octave/piqp_oct.cpp
+++ interfaces/octave/piqp_oct.cpp
@@ -8,7 +8,7 @@
 //
 // LD_PRELOAD=/home/redstone/tmp/piqp/interfaces/c/libpiqpc.so octave
 // autoload("piqp_dense", "/home/redstone/tmp/piqp/interfaces/octave/piqp.oct")
-// [status, x, numiter, obj] = piqp_dense([1 0 ; 0 1], [-4 ; -6], [], [], [], [], [-Inf ; -Inf], [Inf; Inf])
+// [res] = piqp_dense([1 0 ; 0 1], [-4 ; -6], [], [], [], [], [-Inf ; -Inf], [Inf; Inf], struct("verbose", true))
 
 //static_assert(sizeof(piqp_float) == 8);
 
@@ -21,7 +21,7 @@ ColumnVector eigen3VecToCol(const Eigen::Matrix<double, Eigen::Dynamic, 1>& vec)
 }
 
 DEFUN_DLD (piqp_dense, args, nargout,
-           "piqp_dense(Q, c, A, b, G, h, x_lb, x_ub)")
+           "piqp_dense(Q, c, A, b, G, h, x_lb, x_ub, opts)")
 {
   octave_stdout << "piqp_dense has "
                 << args.length () << " input arguments and "
@@ -31,7 +31,7 @@ DEFUN_DLD (piqp_dense, args, nargout,
   for (int i = 0; i < nargout; i++)
     retval(i) = octave_value (Matrix ());
 
-  if (args.length() != 8) {
+  if (args.length() < 8) {
     error("piqp_dense: Incorrect # of args#");
     return retval;
   }
@@ -59,18 +59,55 @@ DEFUN_DLD (piqp_dense, args, nargout,
   Eigen::VectorXd h = Eigen::Map<Eigen::VectorXd>(args(5).vector_value().fortran_vec(), nineq, 1);
   Eigen::VectorXd x_lb = Eigen::Map<Eigen::VectorXd>(args(6).vector_value().fortran_vec(), n, 1);
   Eigen::VectorXd x_ub = Eigen::Map<Eigen::VectorXd>(args(7).vector_value().fortran_vec(), n, 1);
-  //ColumnVector x0  (args(0).vector_value ());
-  //Matrix        Q  (args(1).matrix_value ());
 
   piqp::DenseSolver<double> solver;
-  solver.settings().verbose = true;
-  solver.settings().compute_timings = false;
+
+  if (args.length() == 9) {
+    const octave_scalar_map& opts = args(8).scalar_map_value();
+#define DFIELD(a) { if (opts.contains(#a)) {                            \
+        solver.settings().a = opts.getfield(#a).double_value(); } }
+#define BFIELD(a) { if (opts.contains(#a)) {                            \
+        solver.settings().a = opts.getfield(#a).bool_value(); } }
+#define IFIELD(a) { if (opts.contains(#a)) {                            \
+        solver.settings().a = opts.getfield(#a).int_value(); } }
+    DFIELD(rho_init);
+    DFIELD(delta_init);
+    DFIELD(eps_abs);
+    DFIELD(eps_rel);
+    BFIELD(check_duality_gap);
+    DFIELD(eps_duality_gap_abs);
+    DFIELD(eps_duality_gap_rel);
+    DFIELD(reg_lower_limit);
+    DFIELD(reg_finetune_lower_limit);
+    IFIELD(reg_finetune_primal_update_threshold);
+    IFIELD(reg_finetune_dual_update_threshold);
+    IFIELD(max_iter);
+    IFIELD(max_factor_retires);
+    BFIELD(preconditioner_scale_cost);
+    IFIELD(preconditioner_iter);
+    DFIELD(tau);
+
+    BFIELD(iterative_refinement_always_enabled);
+    DFIELD(iterative_refinement_eps_abs);
+    DFIELD(iterative_refinement_eps_rel);
+    IFIELD(iterative_refinement_max_iter);
+    DFIELD(iterative_refinement_min_improvement_rate);
+    DFIELD(iterative_refinement_static_regularization_eps);
+    DFIELD(iterative_refinement_static_regularization_rel);
+
+    BFIELD(verbose);
+    BFIELD(compute_timings);
+#undef BFIELD
+#undef DFIELD
+#undef IFIELD
+  }
+
   solver.setup(Q, c, A, b, G, h, x_lb, x_ub);
   
   piqp::Status status = solver.solve();
   
-  std::cout << "status = " << status << std::endl;
-  std::cout << "x = " << solver.result().x.transpose() << std::endl;
+  //std::cout << "status = " << status << std::endl;
+  //std::cout << "x = " << solver.result().x.transpose() << std::endl;
 
   //double* xraw = new double[n];
   //memcpy(xraw, solver.result().x.data(), sizeof(double)*n);
