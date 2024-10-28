519ea99c080c3dc2f9d38e41357686b02c2e0783
Roland Schwan <roland.schwan@mikrounix.com>
2024-10-28 16:13:52 +0100

    update sqp benchmarks

--- benchmarks/src/sqp_benchmarks.cpp
+++ benchmarks/src/sqp_benchmarks.cpp
@@ -41,11 +41,28 @@ static void BM_CHAIN_MASS_SQP_KKT_ALL_ELIMINATED(benchmark::State& state)
     }
 }
 
+static void BM_CHAIN_MASS_SQP_BLOCKSPARSE_STAGE_KKT(benchmark::State& state)
+{
+    piqp::sparse::Model<T, I> model = piqp::load_sparse_model<T, I>("data/chain_mass_sqp.mat");
+
+    piqp::SparseSolver<T, I> solver;
+    solver.settings().kkt_solver = piqp::KKTSolver::blocksparse_stagewise;
+    solver.setup(model.P, model.c, model.A, model.b, model.G, model.h, model.x_lb, model.x_ub);
+
+    for (auto _ : state)
+    {
+        solver.update(model.P, model.c, model.A, model.b, model.G, model.h, model.x_lb, model.x_ub);
+        solver.solve();
+    }
+}
+
 static void BM_ROBOT_ARM_SQP_KKT_FULL(benchmark::State& state)
 {
     piqp::sparse::Model<T, I> model = piqp::load_sparse_model<T, I>("data/robot_arm_sqp.mat");
 
     piqp::SparseSolver<T, I, piqp::KKTMode::KKT_FULL> solver;
+    solver.settings().reg_lower_limit = 1e-8;
+    solver.settings().reg_finetune_lower_limit = 1e-8;
     solver.setup(model.P, model.c, model.A, model.b, model.G, model.h, model.x_lb, model.x_ub);
 
     for (auto _ : state)
@@ -60,6 +77,8 @@ static void BM_ROBOT_ARM_SQP_KKT_ALL_ELIMINATED(benchmark::State& state)
     piqp::sparse::Model<T, I> model = piqp::load_sparse_model<T, I>("data/robot_arm_sqp.mat");
 
     piqp::SparseSolver<T, I, piqp::KKTMode::KKT_ALL_ELIMINATED> solver;
+    solver.settings().reg_lower_limit = 1e-8;
+    solver.settings().reg_finetune_lower_limit = 1e-8;
     solver.setup(model.P, model.c, model.A, model.b, model.G, model.h, model.x_lb, model.x_ub);
 
     for (auto _ : state)
@@ -69,9 +88,29 @@ static void BM_ROBOT_ARM_SQP_KKT_ALL_ELIMINATED(benchmark::State& state)
     }
 }
 
-//BENCHMARK(BM_CHAIN_MASS_SQP_KKT_FULL)->Unit(benchmark::kMicrosecond);
-//BENCHMARK(BM_CHAIN_MASS_SQP_KKT_ALL_ELIMINATED)->Unit(benchmark::kMicrosecond);
+static void BM_ROBOT_ARM_SQP_BLOCKSPARSE_STAGE_KKT(benchmark::State& state)
+{
+    piqp::sparse::Model<T, I> model = piqp::load_sparse_model<T, I>("data/robot_arm_sqp.mat");
+
+    piqp::SparseSolver<T, I> solver;
+    solver.settings().kkt_solver = piqp::KKTSolver::blocksparse_stagewise;
+    solver.settings().reg_lower_limit = 1e-8;
+    solver.settings().reg_finetune_lower_limit = 1e-8;
+    solver.setup(model.P, model.c, model.A, model.b, model.G, model.h, model.x_lb, model.x_ub);
+
+    for (auto _ : state)
+    {
+        solver.update(model.P, model.c, model.A, model.b, model.G, model.h, model.x_lb, model.x_ub);
+        solver.solve();
+    }
+}
+
+BENCHMARK(BM_CHAIN_MASS_SQP_KKT_FULL)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_CHAIN_MASS_SQP_KKT_ALL_ELIMINATED)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_CHAIN_MASS_SQP_BLOCKSPARSE_STAGE_KKT)->Unit(benchmark::kMicrosecond);
+
 BENCHMARK(BM_ROBOT_ARM_SQP_KKT_FULL)->Unit(benchmark::kMicrosecond);
-//BENCHMARK(BM_ROBOT_ARM_SQP_KKT_ALL_ELIMINATED)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_ROBOT_ARM_SQP_KKT_ALL_ELIMINATED)->Unit(benchmark::kMicrosecond);
+BENCHMARK(BM_ROBOT_ARM_SQP_BLOCKSPARSE_STAGE_KKT)->Unit(benchmark::kMicrosecond);
 
 BENCHMARK_MAIN();
