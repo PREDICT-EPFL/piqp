ab330de1ad057dcbbc05c0606ba33277d8bddb40
Roland Schwan <roland.schwan@mikrounix.com>
2025-02-22 21:45:21 +0100

    use correct script directory

--- ci/install_all_deps_linux.sh
+++ ci/install_all_deps_linux.sh
@@ -7,21 +7,23 @@
 # This source code is licensed under the BSD 2-Clause License found in the
 # LICENSE file in the root directory of this source tree.
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p build_external
 cd build_external
 
-bash ../ci/install_eigen.sh
+bash "$SCRIPT_DIR/install_eigen.sh"
 
-bash ../ci/download_blasfeo.sh
-bash ../ci/build_install_blasfeo.sh GENERIC generic
+bash "$SCRIPT_DIR/download_blasfeo.sh"
+bash "$SCRIPT_DIR/build_install_blasfeo.sh GENERIC generic"
 
 case $(uname -m) in
     x86_64)
-        bash ../ci/build_install_blasfeo.sh X64_INTEL_CORE x64 /opt/blasfeo_x64
-        bash ../ci/build_install_blasfeo.sh X64_INTEL_HASWELL x64_avx2 /opt/blasfeo_x64_avx2
-        bash ../ci/build_install_blasfeo.sh X64_INTEL_SKYLAKE_X arm64 /opt/blasfeo_x64_avx512
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh X64_INTEL_CORE x64 /opt/blasfeo_x64"
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh X64_INTEL_HASWELL x64_avx2 /opt/blasfeo_x64_avx2"
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh X64_INTEL_SKYLAKE_X arm64 /opt/blasfeo_x64_avx512"
         ;;
     aarch64|arm64)
-        bash ../ci/build_install_blasfeo.sh ARMV8A_ARM_CORTEX_A76 arm64 /opt/blasfeo_arm64
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh ARMV8A_ARM_CORTEX_A76 arm64 /opt/blasfeo_arm64"
         ;;
 esac
--- ci/install_all_deps_windows.sh
+++ ci/install_all_deps_windows.sh
@@ -7,13 +7,15 @@
 # This source code is licensed under the BSD 2-Clause License found in the
 # LICENSE file in the root directory of this source tree.
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 mkdir -p build_external
 cd build_external
 
-bash ../ci/install_eigen.sh
+bash "$SCRIPT_DIR/install_eigen.sh"
 
-bash ../ci/download_blasfeo.sh
-bash ../ci/build_install_blasfeo.sh GENERIC generic
-bash ../ci/build_install_blasfeo.sh X64_INTEL_CORE x64 c:/opt/blasfeo_x64
-bash ../ci/build_install_blasfeo.sh X64_INTEL_HASWELL x64_avx2 c:/opt/blasfeo_x64_avx2
-bash ../ci/build_install_blasfeo.sh X64_INTEL_SKYLAKE_X arm64 c:/opt/blasfeo_x64_avx512
+bash "$SCRIPT_DIR/download_blasfeo.sh"
+bash "$SCRIPT_DIR/build_install_blasfeo.sh GENERIC generic"
+bash "$SCRIPT_DIR/build_install_blasfeo.sh X64_INTEL_CORE x64 c:/opt/blasfeo_x64"
+bash "$SCRIPT_DIR/build_install_blasfeo.sh X64_INTEL_HASWELL x64_avx2 c:/opt/blasfeo_x64_avx2"
+bash "$SCRIPT_DIR/build_install_blasfeo.sh X64_INTEL_SKYLAKE_X arm64 c:/opt/blasfeo_x64_avx512"
