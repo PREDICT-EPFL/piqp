a6b2fd965200d1c568c55d7a893a20f928602377
Roland Schwan <roland.schwan@mikrounix.com>
2025-04-17 23:05:07 +0200

    regenerate maros data to have the correct binary format

--- tests/data/maros_meszaros/convert_osqp_to_piqp.m
+++ tests/data/maros_meszaros/convert_osqp_to_piqp.m
@@ -28,17 +28,20 @@ for file = files'
     eq_bounds = cu == cl;
     ineq_bounds = ~eq_bounds;
 
-    A = C(eq_bounds, :);
+    A = C(eq_bounds, :)'';
     b = cu(eq_bounds);
+    if numel(b) == 0, b = zeros(0, 1); end
 
     G = C(ineq_bounds, :);
     h_u = cu(ineq_bounds);
     h_l = cl(ineq_bounds);
 
     finite_bounds = (h_l > -inf) | (h_u < inf);
-    G = G(finite_bounds, :);
+    G = G(finite_bounds, :)'';
     h_u = h_u(finite_bounds);
     h_l = h_l(finite_bounds);
+    if numel(h_u) == 0, h_u = zeros(0, 1); end
+    if numel(h_l) == 0, h_l = zeros(0, 1); end
 
     save(file.name, "P", "c", "A", "b", "G", "h_l", "h_u", "x_l", "x_u");
 end
