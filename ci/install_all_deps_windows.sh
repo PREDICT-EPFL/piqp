0f6adf0718dc0a4c431a8bceec4a79101e02cddd
Roland Schwan <roland.schwan@mikrounix.com>
2025-04-17 11:28:00 +0200

    fix windows arm build

--- ci/install_all_deps_windows.sh
+++ ci/install_all_deps_windows.sh
@@ -16,6 +16,14 @@ bash "$SCRIPT_DIR/install_eigen.sh"
 
 bash "$SCRIPT_DIR/download_blasfeo.sh"
 bash "$SCRIPT_DIR/build_install_blasfeo.sh" GENERIC generic
-bash "$SCRIPT_DIR/build_install_blasfeo.sh" X64_INTEL_CORE x64 c:/opt/blasfeo_x64
-bash "$SCRIPT_DIR/build_install_blasfeo.sh" X64_INTEL_HASWELL x64_avx2 c:/opt/blasfeo_x64_avx2
-bash "$SCRIPT_DIR/build_install_blasfeo.sh" X64_INTEL_SKYLAKE_X arm64 c:/opt/blasfeo_x64_avx512
+
+case $(uname -m) in
+    x86_64|AMD64)
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh" X64_INTEL_CORE x64 c:/opt/blasfeo_x64
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh" X64_INTEL_HASWELL x64_avx2 c:/opt/blasfeo_x64_avx2
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh" X64_INTEL_SKYLAKE_X x64_avx512 c:/opt/blasfeo_x64_avx512
+        ;;
+    aarch64|arm64|ARM64)
+        bash "$SCRIPT_DIR/build_install_blasfeo.sh" ARMV8A_APPLE_M1 arm64 c:/opt/blasfeo_arm64
+        ;;
+esac
